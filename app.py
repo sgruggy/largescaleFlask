24dc160406aad1b5c973c579eac3935d433fb894
= <=>
2018-12-13 20:30:51 -0500

    app.py updated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# import mongo_stuff
+import mongo_stuff
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route('/search')
 def search():
-    # search_query = request.args.get("query")
-    # messages = mongo_stuff.search_get_messages(search_query)
-    # sorted_messages = mongo_stuff.sorted_messages(messages, search_query)
+    search_query = request.args.get("query")
+    messages = mongo_stuff.search_get_messages(search_query)
+    sorted_messages = mongo_stuff.sorted_messages(messages, search_query)
     return render_template("search.html", message_list = sorted_messages)
